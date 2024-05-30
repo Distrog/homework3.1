ac0c5a848cf218ad4b6cbc6dcac019281f14851f
Dmitriy Stroganov <Distrog@gmail.com>
2024-05-30 22:15:49 +0700

    добавлена связь один ко многим

--- src/main/java/ru/hogwarts/school/model/Faculty.java
+++ src/main/java/ru/hogwarts/school/model/Faculty.java
@@ -1,11 +1,10 @@
 package ru.hogwarts.school.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
+import java.util.List;
 import java.util.Objects;
+
 @Entity
 public class Faculty {
     @Id
@@ -13,6 +12,8 @@ public class Faculty {
     private long id;
     private String name;
     private String color;
+    @OneToMany(mappedBy = "faculty")
+    private List<Student> students;
 
     public long getId() {
         return id;
