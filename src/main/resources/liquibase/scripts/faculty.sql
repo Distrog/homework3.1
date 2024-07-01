f21291962c3e6c0aeea78363c93c97f6abefa92a
Dmitriy Stroganov <distrog@gmail.com>
2024-07-01 21:14:12 +0700

    в проект добавлен liquibase, созданы индексы для таблиц student и faculty

---
+++ src/main/resources/liquibase/scripts/faculty.sql
@@ -0,0 +1,4 @@
+--liquidbase formatted sql
+
+--changeset distrog:2
+create index faculty_name_color_index on faculty (name,color);
---
+++ src/main/resources/liquibase/scripts/student.sql
@@ -0,0 +1,4 @@
+-- liquidbase formatted sql
+
+-- changeset distrog:1
+create index user_name_index on student (name);
